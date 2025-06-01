eeefb755e887af6772ae301d692fbf0f344f4901
MichaelD <dwernicki.michal@gmail.com>
2025-06-02 00:05:20 +0200

    Refactor(PointOfInterestUseCases): Remove unnecessary InsertPointOfInterestUseCase and dispatch IO operations to repository
    - Removed InsertPointOfInterestUseCase as it's not being used.
    - Modified other PointOfInterest use cases to dispatch IO operations to the repository, improving performance and adherence to best practices.

--- app/src/main/java/com/rabbitv/valheimviki/domain/use_cases/point_of_interest/get_point_of_interest_by_subcategory_and_id/GetPointOfInterestBySubCategoryAndIdUseCase.kt
+++ app/src/main/java/com/rabbitv/valheimviki/domain/use_cases/point_of_interest/get_point_of_interest_by_subcategory_and_id/GetPointOfInterestBySubCategoryAndIdUseCase.kt
@@ -1,16 +1,18 @@
 package com.rabbitv.valheimviki.domain.use_cases.point_of_interest.get_point_of_interest_by_subcategory_and_id
 
-import com.rabbitv.valheimviki.domain.exceptions.PointOfInterestBySubCategoryAndIdFetchLocalException
 import com.rabbitv.valheimviki.domain.model.point_of_interest.PointOfInterest
 import com.rabbitv.valheimviki.domain.repository.PointOfInterestRepository
 import jakarta.inject.Inject
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
 
-class GetPointOfInterestBySubCategoryAndIdUseCase@Inject constructor(
+class GetPointOfInterestBySubCategoryAndIdUseCase @Inject constructor(
     private val repository: PointOfInterestRepository
 ) {
-    operator fun invoke(subCategory: String,id:String): Flow<PointOfInterest?> {
-        return repository.getPointOfInterestBySubCategoryAndId(subCategory,id)
+    operator fun invoke(subCategory: String, id: String): Flow<PointOfInterest?> {
+        return repository.getPointOfInterestBySubCategoryAndId(subCategory, id)
+            .flowOn(Dispatchers.IO)
 
     }
 }
--- app/src/main/java/com/rabbitv/valheimviki/domain/use_cases/point_of_interest/get_point_of_interests_by_ids/GetPointsOfInterestByIdsUseCase.kt
+++ app/src/main/java/com/rabbitv/valheimviki/domain/use_cases/point_of_interest/get_point_of_interests_by_ids/GetPointsOfInterestByIdsUseCase.kt
@@ -1,16 +1,17 @@
 package com.rabbitv.valheimviki.domain.use_cases.point_of_interest.get_point_of_interests_by_ids
 
-import com.rabbitv.valheimviki.domain.exceptions.PointOfInterestByIdsFetchLocalException
 import com.rabbitv.valheimviki.domain.model.point_of_interest.PointOfInterest
 import com.rabbitv.valheimviki.domain.repository.PointOfInterestRepository
 import jakarta.inject.Inject
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
 
 class GetPointsOfInterestByIdsUseCase @Inject constructor(
     private val repository: PointOfInterestRepository
 ) {
     operator fun invoke(ids: List<String>): Flow<List<PointOfInterest>> {
-        return repository.getPointOfInterestsByIds(ids)
+        return repository.getPointOfInterestsByIds(ids).flowOn(Dispatchers.IO)
 
     }
 }
