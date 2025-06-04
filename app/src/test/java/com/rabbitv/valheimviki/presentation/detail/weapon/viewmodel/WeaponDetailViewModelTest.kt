d45f4a7692e3d721ae6dc4c64f5ab984c0891157
MichaelD <dwernicki.michal@gmail.com>
2025-06-04 23:51:39 +0200

    Test(WeaponDetailViewModel): Add placeholder tests
    - Added placeholder tests for WeaponDetailViewModel.
    - Current tests are empty and marked with TODO.

---
+++ app/src/test/java/com/rabbitv/valheimviki/presentation/detail/weapon/viewmodel/WeaponDetailViewModelTest.kt
@@ -0,0 +1,28 @@
+package com.rabbitv.valheimviki.presentation.detail.weapon.viewmodel
+
+import org.junit.jupiter.api.Assertions.*
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+
+class WeaponDetailViewModelTest {
+    @BeforeTest
+    fun setUp() {
+        TODO("Not yet implemented")
+    }
+
+    @AfterTest
+    fun tearDown() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun getUiState() {
+    }
+
+    @Test
+    fun loadWeaponData$Translator__v1_debug()
+    {
+    }
+
+}
