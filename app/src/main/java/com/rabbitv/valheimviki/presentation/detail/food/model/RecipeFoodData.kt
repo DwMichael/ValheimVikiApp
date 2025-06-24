3f3faca0a0983467de9e535b6a49a2cffed526c1
RM <dwernicki.miochal@gmail.com>
2025-06-24 19:01:48 +0200

    Added droppableType to Recipe data classes

--- app/src/main/java/com/rabbitv/valheimviki/presentation/detail/food/model/RecipeFoodData.kt
+++ app/src/main/java/com/rabbitv/valheimviki/presentation/detail/food/model/RecipeFoodData.kt
@@ -1,10 +1,12 @@
 package com.rabbitv.valheimviki.presentation.detail.food.model
 
 import com.rabbitv.valheimviki.domain.model.food.Food
+import com.rabbitv.valheimviki.domain.model.presentation.DroppableType
 import com.rabbitv.valheimviki.domain.repository.Droppable
 
 data class RecipeFoodData(
 	override val itemDrop: Food,
 	override val quantityList: List<Int?> = emptyList(),
 	override val chanceStarList: List<Int?> = emptyList(),
+	override val droppableType: DroppableType = DroppableType.FOOD
 ) : Droppable
