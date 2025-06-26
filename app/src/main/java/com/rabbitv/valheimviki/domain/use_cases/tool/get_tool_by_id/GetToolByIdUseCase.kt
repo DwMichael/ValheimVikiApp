71cdcd57ae8bdc9c8871a4cddde802fef79e1584
RM <dwernicki.miochal@gmail.com>
2025-06-26 16:43:49 +0200

    Refactor: Rename GameTool to ItemTool and update related use cases
    - Renamed the data class `GameTool` to `ItemTool` within the `item_tool` package.
    - Added optional `howToUse` and `generalInfo` fields to the `ItemTool` data class.
    - Updated the return types in the following use cases to reflect the name change from `GameTool` to `ItemTool`:
        - `GetToolByIdUseCase`
        - `GetToolsByIdsUseCase`
        - `GetToolsBySubCategoryUseCase`
        - `GetLocalToolsUseCase`
    - Changed the import path for `ToolsUpgradeInfo` within `ItemTool.kt` from `com.rabbitv.valheimviki.domain.model.item_tool.pickaxe.ToolsUpgradeInfo` to `com.rabbitv.valheimviki.domain.model.item_tool.tool_upgrade_info.ToolsUpgradeInfo`.

--- app/src/main/java/com/rabbitv/valheimviki/domain/use_cases/tool/get_tool_by_id/GetToolByIdUseCase.kt
+++ app/src/main/java/com/rabbitv/valheimviki/domain/use_cases/tool/get_tool_by_id/GetToolByIdUseCase.kt
@@ -1,6 +1,6 @@
 package com.rabbitv.valheimviki.domain.use_cases.tool.get_tool_by_id
 
-import com.rabbitv.valheimviki.domain.model.item_tool.GameTool
+import com.rabbitv.valheimviki.domain.model.item_tool.ItemTool
 import com.rabbitv.valheimviki.domain.repository.ToolRepository
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -8,6 +8,6 @@ import javax.inject.Inject
 class GetToolByIdUseCase @Inject constructor(
 	private val toolRepository: ToolRepository
 ) {
-	operator fun invoke(id: String): Flow<List<GameTool>> = toolRepository.getToolById(id)
+	operator fun invoke(id: String): Flow<ItemTool?> = toolRepository.getToolById(id)
 
 }
--- app/src/main/java/com/rabbitv/valheimviki/domain/use_cases/tool/get_tools_by_ids/GetToolsByIdsUseCase.kt
+++ app/src/main/java/com/rabbitv/valheimviki/domain/use_cases/tool/get_tools_by_ids/GetToolsByIdsUseCase.kt
@@ -1,6 +1,6 @@
 package com.rabbitv.valheimviki.domain.use_cases.tool.get_tools_by_ids
 
-import com.rabbitv.valheimviki.domain.model.item_tool.GameTool
+import com.rabbitv.valheimviki.domain.model.item_tool.ItemTool
 import com.rabbitv.valheimviki.domain.repository.ToolRepository
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -8,6 +8,6 @@ import javax.inject.Inject
 class GetToolsByIdsUseCase @Inject constructor(
 	private val toolRepository: ToolRepository
 ) {
-	operator fun invoke(ids: List<String>): Flow<List<GameTool>> = toolRepository.getToolsByIds(ids)
+	operator fun invoke(ids: List<String>): Flow<List<ItemTool>> = toolRepository.getToolsByIds(ids)
 
 }
