583660c2c2da74161ca5d354410f8820c488b88f
RM <dwernicki.miochal@gmail.com>
2025-07-03 21:03:09 +0200

    Build: Remove Material Design dependency
    The `com.google.android.material:material` dependency has been removed from the project.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -95,7 +95,7 @@ dependencies {
 	ksp("com.google.dagger:hilt-android-compiler:2.56.2")
 	implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
 
-	implementation("com.google.android.material:material:1.12.0")
+	
 	implementation("androidx.constraintlayout:constraintlayout:2.2.1")
 	implementation("com.composables:icons-lucide:1.1.0")
 
