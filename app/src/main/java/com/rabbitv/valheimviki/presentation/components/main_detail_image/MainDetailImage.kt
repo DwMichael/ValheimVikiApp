de12f3fa03324a653f23fd25dfc4dee44205c0b4
RM <dwernicki.miochal@gmail.com>
2025-07-03 13:42:45 +0200

    Refactor: Simplify `MainDetailImage` component
    - Removed the `onBack` parameter and the associated back button functionality.
    - Removed `AnimatedVisibility` for the back button.
    - Deleted unused imports related to animations and icons.

--- app/src/main/java/com/rabbitv/valheimviki/presentation/components/main_detail_image/MainDetailImage.kt
+++ app/src/main/java/com/rabbitv/valheimviki/presentation/components/main_detail_image/MainDetailImage.kt
@@ -1,11 +1,6 @@
 package com.rabbitv.valheimviki.presentation.components.main_detail_image
 
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalSharedTransitionApi
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -13,27 +8,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.rounded.ArrowBack
-import androidx.compose.material3.FilledIconButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalInputModeManager
 import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -45,101 +29,67 @@ import coil3.compose.AsyncImage
 import coil3.request.ImageRequest
 import coil3.request.crossfade
 import com.rabbitv.valheimviki.R
-import com.rabbitv.valheimviki.ui.theme.BODY_CONTENT_PADDING
-import com.rabbitv.valheimviki.ui.theme.ForestGreen10Dark
-import com.rabbitv.valheimviki.ui.theme.PrimaryWhite
 import com.rabbitv.valheimviki.ui.theme.ValheimVikiAppTheme
 import com.rabbitv.valheimviki.utils.FakeData.generateFakeMaterials
-import kotlinx.coroutines.delay
 
 @Composable
 fun MainDetailImage(
-    onBack: () -> Unit = {},
-    imageUrl: String,
-    name: String,
-    textAlign: TextAlign = TextAlign.Start,
-    imageScale: Float = 1.0f,
-    contentScale :ContentScale = ContentScale.Crop,
-    backgroundImageColor: Color = Color.Transparent
+	imageUrl: String,
+	name: String,
+	textAlign: TextAlign = TextAlign.Start,
+	imageScale: Float = 1.0f,
+	contentScale: ContentScale = ContentScale.Crop,
+	backgroundImageColor: Color = Color.Transparent
 ) {
-    val backButtonVisibleState = remember { mutableStateOf(false) }
-
-    LaunchedEffect(Unit) {
-        delay(450)
-        backButtonVisibleState.value = true
-    }
-    Box(
-        modifier = Modifier.background(backgroundImageColor)
+	Box(
+		modifier = Modifier
+            .background(backgroundImageColor)
             .height(320.dp),
-        ) {
-        AsyncImage(
-            modifier = Modifier
+	) {
+		AsyncImage(
+			modifier = Modifier
                 .align(Alignment.Center)
                 .fillMaxSize(fraction = imageScale),
-            model = ImageRequest.Builder(LocalContext.current)
-                .data(imageUrl)
-                .crossfade(true)
-                .build(),
-            contentDescription = stringResource(R.string.item_grid_image),
-            error = if(LocalInspectionMode.current)  painterResource(R.drawable.testweapon) else null,
-            contentScale = contentScale,
-        )
-        Surface(
-            modifier = Modifier
+			model = ImageRequest.Builder(LocalContext.current)
+				.data(imageUrl)
+				.crossfade(true)
+				.build(),
+			contentDescription = stringResource(R.string.item_grid_image),
+			error = if (LocalInspectionMode.current) painterResource(R.drawable.testweapon) else null,
+			contentScale = contentScale,
+		)
+		Surface(
+			modifier = Modifier
                 .align(Alignment.BottomCenter)
                 .fillMaxHeight(0.25f)
                 .fillMaxWidth(),
-            tonalElevation = 0.dp,
-            color = Color.Black.copy(alpha = ContentAlpha.medium),
-        ) {
-            Text(
-                modifier = Modifier
+			tonalElevation = 0.dp,
+			color = Color.Black.copy(alpha = ContentAlpha.medium),
+		) {
+			Text(
+				modifier = Modifier
                     .padding
                         (horizontal = 8.dp)
                     .wrapContentHeight(Alignment.CenterVertically),
-                textAlign = textAlign,
-                text = name,
-                color = Color.White,
-                style = MaterialTheme.typography.displaySmall,
-            )
+				textAlign = textAlign,
+				text = name,
+				color = Color.White,
+				style = MaterialTheme.typography.displaySmall,
+			)
 
-        }
-        AnimatedVisibility(
-            visible = backButtonVisibleState.value,
-            enter = fadeIn(),
-            exit = fadeOut(),
-            modifier = Modifier
-                .align(Alignment.TopStart)
-                .padding(16.dp)
-        ) {
-            FilledIconButton(
-                onClick = onBack,
-                shape = RoundedCornerShape(12.dp),
-                colors = IconButtonDefaults.filledIconButtonColors(
-                    containerColor = ForestGreen10Dark,
-                ),
-                modifier = Modifier.size(56.dp)
-            ) {
-                Icon(
-                    Icons.AutoMirrored.Rounded.ArrowBack,
-                    contentDescription = "Back",
-                    tint = Color.White
-                )
-            }
-        }
-    }
+		}
+	}
 }
 
 @OptIn(ExperimentalSharedTransitionApi::class)
 @Preview(showBackground = true)
 @Composable
 fun PreviewMainDetailImage() {
-    ValheimVikiAppTheme {
-        generateFakeMaterials()
-        MainDetailImage(
-            onBack = {},
-            imageUrl = "ss",
-            name = "Boar"
-        )
-    }
+	ValheimVikiAppTheme {
+		generateFakeMaterials()
+		MainDetailImage(
+			imageUrl = "ss",
+			name = "Boar"
+		)
+	}
 }
