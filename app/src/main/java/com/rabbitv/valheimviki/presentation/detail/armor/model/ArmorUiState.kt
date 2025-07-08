adef45b16e7a252ec8b0029a9819d1b89a32a3f2
RM <dwernicki.miochal@gmail.com>
2025-07-08 18:40:24 +0200

    Added FavoriteUseCase to ArmorDetailScreen.kt
    - Implemented FavoriteButton and toggleFavorite in VM
    - All work

--- app/src/main/java/com/rabbitv/valheimviki/presentation/detail/armor/model/ArmorUiState.kt
+++ app/src/main/java/com/rabbitv/valheimviki/presentation/detail/armor/model/ArmorUiState.kt
@@ -1,15 +1,14 @@
-package com.rabbitv.valheimviki.presentation.detail.weapon.model
+package com.rabbitv.valheimviki.presentation.detail.armor.model
 
 import com.rabbitv.valheimviki.domain.model.armor.Armor
 import com.rabbitv.valheimviki.domain.model.crafting_object.CraftingObject
-import com.rabbitv.valheimviki.domain.model.food.FoodAsMaterialUpgrade
 import com.rabbitv.valheimviki.domain.model.material.MaterialUpgrade
-import com.rabbitv.valheimviki.domain.model.weapon.Weapon
 
 data class ArmorUiState(
     val armor: Armor? = null,
     val materials: List<MaterialUpgrade> = emptyList(),
-    val craftingObjects: CraftingObject? = null,
+    val craftingObject: CraftingObject? = null,
+    val isFavorite: Boolean = false,
     val isLoading: Boolean = false,
     val error: String? = null
 )
