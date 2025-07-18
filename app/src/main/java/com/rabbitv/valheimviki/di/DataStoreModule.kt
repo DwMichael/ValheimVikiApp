4e7f60f58d9a2296195238cf52ac2062d4f1679a
RM <dwernicki.miochal@gmail.com>
2025-07-18 21:15:42 +0200

    Added DataStoreModule.kt

---
+++ app/src/main/java/com/rabbitv/valheimviki/di/DataStoreModule.kt
@@ -0,0 +1,23 @@
+package com.rabbitv.valheimviki.di
+
+import android.content.Context
+import com.rabbitv.valheimviki.data.repository.DataStoreOperationsImpl
+import com.rabbitv.valheimviki.domain.repository.DataStoreOperations
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataStoreModule {
+	@Provides
+	@Singleton
+	fun bindDataStoreOperationsImpl(
+		@ApplicationContext context: Context
+	): DataStoreOperations {
+		return DataStoreOperationsImpl(context = context)
+	}
+}
