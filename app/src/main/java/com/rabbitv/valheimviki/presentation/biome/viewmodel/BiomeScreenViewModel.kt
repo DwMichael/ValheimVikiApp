d4f6fe521f945ddf4cc2bb478da4e18a4e2ac1c8
RM <dwernicki.miochal@gmail.com>
2025-07-19 10:48:17 +0200

    Refactor : BiomeViewModel
    - Changed from UiListState to UIState<List<Biome>>
    - Changed inside of combine 2 flows fun
    - Deleted onStart "Not needed"
    - Added onCompletion for debug

--- app/src/main/java/com/rabbitv/valheimviki/presentation/biome/viewmodel/BiomeScreenViewModel.kt
+++ app/src/main/java/com/rabbitv/valheimviki/presentation/biome/viewmodel/BiomeScreenViewModel.kt
@@ -3,9 +3,9 @@ package com.rabbitv.valheimviki.presentation.biome.viewmodel
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.rabbitv.valheimviki.R.string.error_no_connection_with_empty_list_message
 import com.rabbitv.valheimviki.domain.model.biome.Biome
-import com.rabbitv.valheimviki.domain.model.ui_state.default_list_state.ErrorType
-import com.rabbitv.valheimviki.domain.model.ui_state.default_list_state.UiListState
+import com.rabbitv.valheimviki.domain.model.ui_state.uistate.UIState
 import com.rabbitv.valheimviki.domain.repository.NetworkConnectivity
 import com.rabbitv.valheimviki.domain.use_cases.biome.BiomeUseCases
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -13,7 +13,7 @@ import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.stateIn
 import java.io.IOException
 import javax.inject.Inject
@@ -21,47 +21,31 @@ import javax.inject.Inject
 
 @HiltViewModel
 class BiomeScreenViewModel @Inject constructor(
-    private val biomeUseCases: BiomeUseCases,
-    private val connectivityObserver: NetworkConnectivity,
+	private val biomeUseCases: BiomeUseCases,
+	private val connectivityObserver: NetworkConnectivity,
 ) : ViewModel() {
 
-    val biomeUiListState: StateFlow<UiListState<Biome>> = combine(
-        biomeUseCases.getLocalBiomesUseCase(),
-        connectivityObserver.isConnected.stateIn(
-            scope = viewModelScope,
-            started = SharingStarted.Companion.WhileSubscribed(5000),
-            initialValue = true
-        ),
-    ) { biomes, isConnected ->
-        if (isConnected) {
-            if (biomes.isNotEmpty()) {
-                UiListState.Success(biomes)
-            } else {
-                UiListState.Loading
-            }
-        } else {
-            if (biomes.isNotEmpty()) {
-                UiListState.Success(biomes)
-            } else {
-                UiListState.Error(
-                    "No internet connection and no local data available. Try to connect to the internet again.",
-                    ErrorType.INTERNET_CONNECTION
-                )
-            }
-        }
-    }.onStart {
-        emit(UiListState.Loading)
-    }.catch { e ->
-        val errorMessage = when (e) {
-            is IOException -> "Problem accessing local data."
-            else -> "An unexpected error occurred."
-        }
-        emit(UiListState.Error(errorMessage, ErrorType.UNKNOWN_ERROR))
-        Log.e("BiomeScreenVM", "Error in biomeUiState flow", e)
-    }.stateIn(
-        scope = viewModelScope,
-        started = SharingStarted.WhileSubscribed(3000),
-        initialValue = UiListState.Loading
-    )
+	val biomeUiListState: StateFlow<UIState<List<Biome>>> = combine(
+		biomeUseCases.getLocalBiomesUseCase(),
+		connectivityObserver.isConnected,
+	) { biomes, isConnected ->
+		when {
+			biomes.isNotEmpty() -> UIState.Success(biomes)
+			isConnected -> UIState.Loading
+			else -> UIState.Error(error_no_connection_with_empty_list_message.toString())
+		}
+	}.onCompletion { error -> println("Error -> ${error?.message}") }
+		.catch { e ->
+			val errorMessage = when (e) {
+				is IOException -> "Problem accessing local data."
+				else -> "An unexpected error occurred."
+			}
+			emit(UIState.Error(errorMessage))
+			Log.e("BiomeScreenVM", "Error in biomeUiState flow", e)
+		}.stateIn(
+			scope = viewModelScope,
+			started = SharingStarted.WhileSubscribed(5000),
+			initialValue = UIState.Loading
+		)
 
 }
