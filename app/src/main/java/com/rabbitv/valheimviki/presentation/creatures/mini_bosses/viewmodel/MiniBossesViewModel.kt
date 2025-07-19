4450adf81ba29c40ad1db0d7031472d49aad7a83
RM <dwernicki.miochal@gmail.com>
2025-07-19 16:59:28 +0200

    Refactor: Migrate MiniBossesViewModel to use UIState
    - Updated `miniBossUiListState` to use the generic `UIState` instead of the specific `UiListState`.
    - Adjusted the logic for emitting UI states based on data availability and network connectivity to align with the new `UIState` structure.
    - Ensured `onStart` emits `UIState.Loading` and error handling correctly emits `UIState.Error`.
    - Added `onCompletion` for logging potential errors.

--- app/src/main/java/com/rabbitv/valheimviki/presentation/creatures/mini_bosses/viewmodel/MiniBossesViewModel.kt
+++ app/src/main/java/com/rabbitv/valheimviki/presentation/creatures/mini_bosses/viewmodel/MiniBossesViewModel.kt
@@ -3,8 +3,9 @@ package com.rabbitv.valheimviki.presentation.creatures.mini_bosses.viewmodel
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.rabbitv.valheimviki.R.string.error_no_connection_with_empty_list_message
 import com.rabbitv.valheimviki.domain.model.creature.mini_boss.MiniBoss
-import com.rabbitv.valheimviki.domain.model.ui_state.default_list_state.UiListState
+import com.rabbitv.valheimviki.domain.model.ui_state.uistate.UIState
 import com.rabbitv.valheimviki.domain.repository.NetworkConnectivity
 import com.rabbitv.valheimviki.domain.use_cases.creature.CreatureUseCases
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -12,51 +13,41 @@ import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
 class MiniBossesViewModel @Inject constructor(
-    private val creatureUseCases: CreatureUseCases,
-    private val connectivityObserver: NetworkConnectivity,
+	val creatureUseCases: CreatureUseCases,
+	val connectivityObserver: NetworkConnectivity,
 ) : ViewModel() {
 
-    val miniBossUiListState: StateFlow<UiListState<MiniBoss>> = combine(
-        creatureUseCases.getMiniBossesUseCase()
-            .catch { e ->
-                Log.e("MiniBossScreenVM", "getLocalCreaturesUseCase failed in combine", e)
-                emit(emptyList())
-            },
-        connectivityObserver.isConnected.stateIn(
-            scope = viewModelScope,
-            started = SharingStarted.Companion.WhileSubscribed(5000),
-            initialValue = true
-        )
-    ) { creatures, isConnected ->
-        if (isConnected) {
-            if (creatures.isNotEmpty()) {
-                UiListState.Success(creatures)
-            } else {
-                UiListState.Loading
-            }
-        } else {
-            if (creatures.isNotEmpty()) {
-                UiListState.Success(creatures)
-            } else {
-                UiListState.Error("No internet connection and no local data available.")
-            }
-        }
-    }.onStart {
-        emit(UiListState.Loading)
-    }.catch { e ->
-        Log.e("MiniBossScreenVM", "Error in creatureUiState flow", e)
-        emit(UiListState.Error(e.message ?: "An unknown error occurred"))
-    }.stateIn(
-        scope = viewModelScope,
-        started = SharingStarted.Companion.WhileSubscribed(3000),
-        initialValue = UiListState.Loading
-    )
+	val miniBossUiListState: StateFlow<UIState<List<MiniBoss>>> = combine(
+		creatureUseCases.getMiniBossesUseCase()
+			.catch { e ->
+				Log.e("MiniBossScreenVM", "getLocalCreaturesUseCase failed in combine", e)
+				emit(emptyList())
+			},
+		connectivityObserver.isConnected.stateIn(
+			scope = viewModelScope,
+			started = SharingStarted.Companion.WhileSubscribed(5000),
+			initialValue = true
+		)
+	) { creatures, isConnected ->
+		when {
+			creatures.isNotEmpty() -> UIState.Success(creatures)
+			isConnected -> UIState.Loading
+			else -> UIState.Error(error_no_connection_with_empty_list_message.toString())
+		}
+	}.onCompletion { error -> println("Error -> ${error?.message}") }.catch { e ->
+		Log.e("MiniBossScreenVM", "Error in creatureUiState flow", e)
+		emit(UIState.Error(e.message ?: "An unknown error occurred"))
+	}.stateIn(
+		scope = viewModelScope,
+		started = SharingStarted.Companion.WhileSubscribed(3000),
+		initialValue = UIState.Loading
+	)
 
 
 }
