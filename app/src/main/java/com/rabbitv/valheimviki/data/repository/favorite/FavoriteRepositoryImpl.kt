cf2be63c2d576f25d9e6a541b704540310d667b7
RM <dwernicki.miochal@gmail.com>
2025-07-23 15:40:06 +0200

    Run DB operations in FavoriteRepositoryImpl on IO dispatcher
    - Injected `IoDispatcher` into `FavoriteRepositoryImpl`.
    - Wrapped DAO calls in `flowOn(ioDispatcher)` for Flow-returning methods (`isFavorite`, `getFavorites`).
    - Used `withContext(ioDispatcher)` for suspend functions (`deleteFavorite`, `addFavorite`) to ensure database operations are performed on the IO thread.

--- app/src/main/java/com/rabbitv/valheimviki/data/repository/favorite/FavoriteRepositoryImpl.kt
+++ app/src/main/java/com/rabbitv/valheimviki/data/repository/favorite/FavoriteRepositoryImpl.kt
@@ -1,27 +1,38 @@
 package com.rabbitv.valheimviki.data.repository.favorite
 
 import com.rabbitv.valheimviki.data.local.dao.FavoriteDao
+import com.rabbitv.valheimviki.di.qualifiers.IoDispatcher
 import com.rabbitv.valheimviki.domain.model.favorite.Favorite
 import com.rabbitv.valheimviki.domain.repository.FavoriteRepository
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class FavoriteRepositoryImpl @Inject constructor(
-	private val favoriteDao: FavoriteDao
-) : FavoriteRepository {
+	private val favoriteDao: FavoriteDao,
+	@param:IoDispatcher private val ioDispatcher: CoroutineDispatcher,
+
+	) : FavoriteRepository {
 	override fun isFavorite(id: String): Flow<Favorite?> {
-		return favoriteDao.isFavorite(id)
+		return favoriteDao.isFavorite(id).flowOn(ioDispatcher)
 	}
 
 	override fun getFavorites(): Flow<List<Favorite>> {
-		return favoriteDao.getAllFavorites()
+		return favoriteDao.getAllFavorites().flowOn(ioDispatcher)
 	}
 
 	override suspend fun deleteFavorite(favorite: Favorite) {
-		return favoriteDao.deleteFavorite(favorite)
+		return withContext(ioDispatcher) {
+			favoriteDao.deleteFavorite(favorite)
+		}
 	}
 
 	override suspend fun addFavorite(favorite: Favorite) {
-		favoriteDao.addFavorite(favorite)
+		withContext(ioDispatcher) {
+			favoriteDao.addFavorite(favorite)
+		}
+
 	}
 }
