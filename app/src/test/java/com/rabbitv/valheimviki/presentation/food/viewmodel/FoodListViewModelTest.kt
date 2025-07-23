357ee3b99119bf433e0ef288c412b9d90a5b890d
RM <dwernicki.miochal@gmail.com>
2025-07-23 17:13:33 +0200

    Test: Add skeleton tests for FoodListViewModel and WelcomeViewModel
    - Created `FoodListViewModelTest.kt` with placeholder `setUp` and `tearDown` methods.
    - Created `WelcomeViewModelTest.kt` with placeholder `setUp` and `tearDown` methods.

---
+++ app/src/test/java/com/rabbitv/valheimviki/presentation/food/viewmodel/FoodListViewModelTest.kt
@@ -0,0 +1,17 @@
+package com.rabbitv.valheimviki.presentation.food.viewmodel
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+
+class FoodListViewModelTest {
+	@BeforeEach
+	fun setUp() {
+		TODO("Not yet implemented")
+	}
+
+	@AfterEach
+	fun tearDown() {
+		TODO("Not yet implemented")
+	}
+
+}
---
+++ app/src/test/java/com/rabbitv/valheimviki/presentation/intro/WelcomeViewModelTest.kt
@@ -0,0 +1,17 @@
+package com.rabbitv.valheimviki.presentation.intro
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+
+class WelcomeViewModelTest {
+	@BeforeEach
+	fun setUp() {
+		TODO("Not yet implemented")
+	}
+
+	@AfterEach
+	fun tearDown() {
+		TODO("Not yet implemented")
+	}
+
+}
