249e5fb2c0ebcad126bd8b19199b853e369a74be
RM <dwernicki.miochal@gmail.com>
2025-07-30 13:27:00 +0200

    Test: Rollback CraftingListViewModel.kt and CraftingListScreenTest.kt changes
    - Added filteredCraftingObjectWithSelection and make it so it return Pair of selectedChip and craftingObject list
    
    - no more race condition

--- app/src/main/java/com/rabbitv/valheimviki/presentation/crafting/viewmodel/CraftingListViewModel.kt
+++ app/src/main/java/com/rabbitv/valheimviki/presentation/crafting/viewmodel/CraftingListViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.rabbitv.valheimviki.R.string.error_no_connection_with_empty_list_message
 import com.rabbitv.valheimviki.di.qualifiers.DefaultDispatcher
+import com.rabbitv.valheimviki.domain.model.crafting_object.CraftingObject
 import com.rabbitv.valheimviki.domain.model.crafting_object.CraftingSubCategory
 import com.rabbitv.valheimviki.domain.model.ui_state.uistate.UIState
 import com.rabbitv.valheimviki.domain.repository.NetworkConnectivity
@@ -13,11 +14,13 @@ import com.rabbitv.valheimviki.presentation.crafting.model.CraftingListUiEvent
 import com.rabbitv.valheimviki.presentation.crafting.model.CraftingListUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import javax.inject.Inject
@@ -30,28 +33,31 @@ class CraftingListViewModel @Inject constructor(
 	@param:DefaultDispatcher private val defaultDispatcher: CoroutineDispatcher
 ) : ViewModel() {
 	private val _selectedChip = MutableStateFlow<CraftingSubCategory?>(null)
+	internal val filteredCraftingObjectWithSelection: Flow<Pair<List<CraftingObject>, CraftingSubCategory?>> =
+		combine(
+			craftingObjectUseCases.getLocalCraftingObjectsUseCase(),
+			_selectedChip,
+		) { allCraftingObjects, chip ->
+			if (chip == null) {
+				return@combine Pair(allCraftingObjects.sortedBy { it.name }, chip)
+			}
+			Pair(allCraftingObjects.filter { it.subCategory == chip.toString() }
+				.sortedBy { it.order }, chip)
+		}.flowOn(defaultDispatcher)
+
 
 	val uiState: StateFlow<CraftingListUiState> = combine(
-		craftingObjectUseCases.getLocalCraftingObjectsUseCase(),
-		_selectedChip,
+		filteredCraftingObjectWithSelection,
 		connectivityObserver.isConnected.stateIn(
 			scope = viewModelScope,
 			started = SharingStarted.Companion.WhileSubscribed(5000),
 			initialValue = true
 		)
-	) { craftingObjects, selectedChip, isConnected ->
-		val filteredCraftingObjects = if (selectedChip == null) {
-			craftingObjects.sortedBy { it.name }
-		} else {
-			craftingObjects.filter { it.subCategory == selectedChip.toString() }
-				.sortedBy { it.order }
-
-		}
-
+	) { (craftingObjects, selectedChip), isConnected ->
 		when {
-			filteredCraftingObjects.isNotEmpty() -> CraftingListUiState(
+			craftingObjects.isNotEmpty() -> CraftingListUiState(
 				selectedChip = selectedChip,
-				craftingListUiState = UIState.Success(filteredCraftingObjects),
+				craftingListUiState = UIState.Success(craftingObjects),
 
 				)
 
@@ -81,7 +87,7 @@ class CraftingListViewModel @Inject constructor(
 			craftingListUiState = UIState.Loading
 		)
 	)
-	
+
 	fun onEvent(event: CraftingListUiEvent) {
 		when (event) {
 			is CraftingListUiEvent.ChipSelected -> {
--- app/src/test/java/com/rabbitv/valheimviki/presentation/crafting/CraftingListScreenTest.kt
+++ app/src/test/java/com/rabbitv/valheimviki/presentation/crafting/CraftingListScreenTest.kt
@@ -3,6 +3,7 @@ package com.rabbitv.valheimviki.presentation.crafting
 import app.cash.turbine.test
 import com.rabbitv.valheimviki.R.string.error_no_connection_with_empty_list_message
 import com.rabbitv.valheimviki.domain.model.crafting_object.CraftingObject
+import com.rabbitv.valheimviki.domain.model.crafting_object.CraftingSubCategory
 import com.rabbitv.valheimviki.domain.model.ui_state.uistate.UIState
 import com.rabbitv.valheimviki.domain.repository.NetworkConnectivity
 import com.rabbitv.valheimviki.domain.use_cases.crafting_object.CraftingObjectUseCases
@@ -11,9 +12,11 @@ import com.rabbitv.valheimviki.domain.use_cases.crafting_object.get_crafting_obj
 import com.rabbitv.valheimviki.domain.use_cases.crafting_object.get_crafting_object_by_sub_category_use_case.GetCraftingObjectsBySubCategoryUseCase
 import com.rabbitv.valheimviki.domain.use_cases.crafting_object.get_crafting_objects_by_ids.GetCraftingObjectsByIdsUseCase
 import com.rabbitv.valheimviki.domain.use_cases.crafting_object.get_local_crafting_object_use_case.GetLocalCraftingObjectsUseCase
+import com.rabbitv.valheimviki.presentation.crafting.model.CraftingListUiEvent
 import com.rabbitv.valheimviki.presentation.crafting.viewmodel.CraftingListViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.resetMain
@@ -28,6 +31,7 @@ import org.mockito.Mock
 import org.mockito.junit.jupiter.MockitoExtension
 import org.mockito.kotlin.whenever
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 import kotlin.test.fail
 
 
@@ -80,6 +84,275 @@ class CraftingListScreenTest {
 	@AfterEach
 	fun tearDown() {
 		Dispatchers.resetMain()
+	}
+
+	@Test
+	fun craftingListViewModel_craftingObjectsFlow_emitsFilteredCraftingObjectsWhenChipIsSelected() =
+		runTest {
+
+			val fakeCraftingObjectList: List<CraftingObject> = listOf(
+				CraftingObject(
+					id = "1",
+					name = "Test Crafting Object",
+					subCategory = CraftingSubCategory.CRAFTING_STATION.toString(),
+					imageUrl = "",
+					category = "",
+					description = "",
+					order = 1
+				),
+				CraftingObject(
+					id = "2",
+					name = "Test Crafting Object",
+					subCategory = CraftingSubCategory.FOOD_CRAFTING.toString(),
+					imageUrl = "",
+					category = "",
+					description = "",
+					order = 2
+				),
+				CraftingObject(
+					id = "3",
+					name = "Test Crafting Object",
+					subCategory = CraftingSubCategory.SMELTING_CRAFTING.toString(),
+					imageUrl = "",
+					category = "",
+					description = "",
+					order = 3
+				),
+				CraftingObject(
+					id = "4",
+					name = "Test Crafting Object",
+					subCategory = CraftingSubCategory.SMELTING_CRAFTING.toString(),
+					imageUrl = "",
+					category = "",
+					description = "",
+					order = 4
+				)
+			)
+
+
+
+			whenever(getLocalCraftingObjectsUseCase.invoke()).thenReturn(
+				flowOf(
+					fakeCraftingObjectList
+				)
+			)
+			val viewModel =
+				CraftingListViewModel(craftingUseCases, connectivityObserver, Dispatchers.Default)
+
+			viewModel.filteredCraftingObjectWithSelection.test {
+
+				assertEquals(4, awaitItem().first.size)
+
+				viewModel.onEvent(CraftingListUiEvent.ChipSelected(CraftingSubCategory.SMELTING_CRAFTING))
+
+				val filteredList = awaitItem()
+				assertEquals(2, filteredList.first.size)
+				assertTrue(filteredList.first.all { it.subCategory == CraftingSubCategory.SMELTING_CRAFTING.toString() })
+			}
+
+		}
+
+	@Test
+	fun craftingListViewModel_craftingObjectsFlow_emitsAllCraftingObjectsWhenNoChipIsSelected() =
+		runTest {
+
+			val fakeCraftingObjectList: List<CraftingObject> = listOf(
+				CraftingObject(
+					id = "1",
+					name = "Test Crafting Object",
+					subCategory = CraftingSubCategory.CRAFTING_STATION.toString(),
+					imageUrl = "",
+					category = "",
+					description = "",
+					order = 1
+				),
+				CraftingObject(
+					id = "2",
+					name = "Test Crafting Object",
+					subCategory = CraftingSubCategory.FOOD_CRAFTING.toString(),
+					imageUrl = "",
+					category = "",
+					description = "",
+					order = 2
+				),
+				CraftingObject(
+					id = "3",
+					name = "Test Crafting Object",
+					subCategory = CraftingSubCategory.SMELTING_CRAFTING.toString(),
+					imageUrl = "",
+					category = "",
+					description = "",
+					order = 3
+				),
+				CraftingObject(
+					id = "4",
+					name = "Test Crafting Object",
+					subCategory = CraftingSubCategory.SMELTING_CRAFTING.toString(),
+					imageUrl = "",
+					category = "",
+					description = "",
+					order = 4
+				)
+			)
+
+			whenever(getLocalCraftingObjectsUseCase.invoke()).thenReturn(
+				flowOf(
+					fakeCraftingObjectList
+				)
+			)
+			val viewModel =
+				CraftingListViewModel(craftingUseCases, connectivityObserver, Dispatchers.Default)
+			viewModel.filteredCraftingObjectWithSelection.test {
+				val armors = awaitItem()
+
+				assertEquals(4, armors.first.size)
+				assertEquals(fakeCraftingObjectList, armors.first)
+			}
+		}
+
+
+	@Test
+	fun craftingListViewModel_craftingObjectsFlow_handlesEmptyCraftingObjectList() = runTest {
+
+		whenever(getLocalCraftingObjectsUseCase.invoke()).thenReturn(flowOf(emptyList()))
+		val viewModel =
+			CraftingListViewModel(craftingUseCases, connectivityObserver, Dispatchers.Default)
+		val result = viewModel.filteredCraftingObjectWithSelection.first()
+		assertTrue(result.first.isEmpty())
+	}
+
+	@Test
+	fun craftingListViewModel_craftingObjectsFlow_handlesChipSelectionChanges() = runTest {
+		val fakeCraftingObjectList: List<CraftingObject> = listOf(
+			CraftingObject(
+				id = "1",
+				name = "Test Crafting Object",
+				subCategory = CraftingSubCategory.CRAFTING_STATION.toString(),
+				imageUrl = "",
+				category = "",
+				description = "",
+				order = 1
+			),
+			CraftingObject(
+				id = "2",
+				name = "Test Crafting Object",
+				subCategory = CraftingSubCategory.FOOD_CRAFTING.toString(),
+				imageUrl = "",
+				category = "",
+				description = "",
+				order = 2
+			),
+			CraftingObject(
+				id = "3",
+				name = "Test Crafting Object",
+				subCategory = CraftingSubCategory.SMELTING_CRAFTING.toString(),
+				imageUrl = "",
+				category = "",
+				description = "",
+				order = 3
+			),
+			CraftingObject(
+				id = "4",
+				name = "Test Crafting Object",
+				subCategory = CraftingSubCategory.SMELTING_CRAFTING.toString(),
+				imageUrl = "",
+				category = "",
+				description = "",
+				order = 4
+			)
+		)
+
+		whenever(getLocalCraftingObjectsUseCase.invoke()).thenReturn(flowOf(fakeCraftingObjectList))
+		val viewModel =
+			CraftingListViewModel(craftingUseCases, connectivityObserver, Dispatchers.Default)
+
+
+		viewModel.filteredCraftingObjectWithSelection.test {
+
+			val initialEmittedList = awaitItem().first
+			assertEquals(4, initialEmittedList.size)
+			assertEquals(fakeCraftingObjectList, initialEmittedList)
+
+
+			viewModel.onEvent(CraftingListUiEvent.ChipSelected(CraftingSubCategory.SMELTING_CRAFTING))
+			val legArmorEmittedList = awaitItem().first
+			assertEquals(
+				2,
+				legArmorEmittedList.size
+			)
+			assertTrue(legArmorEmittedList.all { it.subCategory == CraftingSubCategory.SMELTING_CRAFTING.toString() })
+
+
+			viewModel.onEvent(CraftingListUiEvent.ChipSelected(CraftingSubCategory.CRAFTING_STATION))
+			val chestArmorEmittedList = awaitItem().first
+			assertEquals(
+				1,
+				chestArmorEmittedList.size
+			)
+			assertTrue(chestArmorEmittedList.all { it.subCategory == CraftingSubCategory.CRAFTING_STATION.toString() })
+
+
+			viewModel.onEvent(CraftingListUiEvent.ChipSelected(CraftingSubCategory.CRAFTING_STATION))
+			val allArmorsAgainEmittedList = awaitItem().first
+			assertEquals(
+				4,
+				allArmorsAgainEmittedList.size
+			)
+			assertEquals(fakeCraftingObjectList, allArmorsAgainEmittedList)
+			expectNoEvents()
+			cancelAndIgnoreRemainingEvents()
+		}
+	}
+
+	@Test
+	fun craftingListViewModel_craftingObjectsFlow_SwitchesTODefaultDispatcher() = runTest {
+		val fakeCraftingObjectList: List<CraftingObject> = listOf(
+			CraftingObject(
+				id = "1",
+				name = "Test Crafting Object",
+				subCategory = CraftingSubCategory.CRAFTING_STATION.toString(),
+				imageUrl = "",
+				category = "",
+				description = "",
+				order = 1
+			),
+			CraftingObject(
+				id = "2",
+				name = "Test Crafting Object",
+				subCategory = CraftingSubCategory.FOOD_CRAFTING.toString(),
+				imageUrl = "",
+				category = "",
+				description = "",
+				order = 2
+			),
+			CraftingObject(
+				id = "3",
+				name = "Test Crafting Object",
+				subCategory = CraftingSubCategory.SMELTING_CRAFTING.toString(),
+				imageUrl = "",
+				category = "",
+				description = "",
+				order = 3
+			),
+			CraftingObject(
+				id = "4",
+				name = "Test Crafting Object",
+				subCategory = CraftingSubCategory.SMELTING_CRAFTING.toString(),
+				imageUrl = "",
+				category = "",
+				description = "",
+				order = 4
+			)
+		)
+
+		whenever(getLocalCraftingObjectsUseCase.invoke()).thenReturn(flowOf(fakeCraftingObjectList))
+		val viewModel =
+			CraftingListViewModel(craftingUseCases, connectivityObserver, Dispatchers.Default)
+		val result = viewModel.filteredCraftingObjectWithSelection.first().first
+
+
+		assertEquals(fakeCraftingObjectList, result)
+
 	}
 
 
