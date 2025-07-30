b7e33790d1f56f03b36d2115be5161cfdaec6c87
RM <dwernicki.miochal@gmail.com>
2025-07-30 10:31:02 +0200

    Refactor: Simplify GetLocalMeadsUseCase and sort mead list in ViewModel
    - Removed sorting from `GetLocalMeadsUseCase` as it's now handled in `MeadListViewModel`.
    - `MeadListViewModel` now sorts the mead list by `order` after filtering.
    - Added error handling to the `meadList` flow in `MeadListViewModel`.

--- app/src/main/java/com/rabbitv/valheimviki/presentation/mead/viewmodel/MeadListViewModel.kt
+++ app/src/main/java/com/rabbitv/valheimviki/presentation/mead/viewmodel/MeadListViewModel.kt
@@ -22,6 +22,7 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 
@@ -38,8 +39,10 @@ class MeadListViewModel @Inject constructor(
 		_selectedSubCategory,
 	) { allMead, category ->
 		allMead.filter { it.subCategory == category.toString() }
+			.sortedBy { it.order }
 	}.flowOn(defaultDispatcher)
-
+		.onCompletion { error -> println("Error -> ${error?.message}") }
+		.catch { throw it }
 
 	val uiState: StateFlow<MeadUiSate> = combine(
 		meadList,
