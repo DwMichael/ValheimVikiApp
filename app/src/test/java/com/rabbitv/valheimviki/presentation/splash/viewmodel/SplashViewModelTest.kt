7342b0660bfb8bb5df4be9175bf26984b7ed7389
RM <dwernicki.miochal@gmail.com>
2025-08-02 22:21:59 +0200

    Test: Add tests for SplashViewModel
    - Added `onBoardingCompleted_InitLoad_ShouldBeFalse` to verify initial `onBoardingCompleted` state.
    - Added `onBoardingCompleted_UpdateValue_ShouldBeTrue` to verify `onBoardingCompleted` updates correctly when `readOnBoardingUseCase` emits true.

--- app/src/test/java/com/rabbitv/valheimviki/presentation/splash/viewmodel/SplashViewModelTest.kt
+++ app/src/test/java/com/rabbitv/valheimviki/presentation/splash/viewmodel/SplashViewModelTest.kt
@@ -1,5 +1,6 @@
 package com.rabbitv.valheimviki.presentation.splash.viewmodel
 
+import app.cash.turbine.test
 import com.rabbitv.valheimviki.domain.use_cases.datastore.DataStoreUseCases
 import com.rabbitv.valheimviki.domain.use_cases.datastore.get_onboarding_state.ReadOnBoardingState
 import com.rabbitv.valheimviki.domain.use_cases.datastore.language_state_provider.LanguageProvider
@@ -7,14 +8,20 @@ import com.rabbitv.valheimviki.domain.use_cases.datastore.save_onboarding_state.
 import com.rabbitv.valheimviki.domain.use_cases.datastore.saved_language_state.SaveLanguageState
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
 import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.Mock
 import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.whenever
+import kotlin.test.assertFalse
 
 
 @OptIn(ExperimentalCoroutinesApi::class)
@@ -55,6 +62,38 @@ class SplashViewModelTest {
 	@AfterEach
 	fun tearDown() {
 		Dispatchers.resetMain()
+	}
+
+	@Test
+	fun onBoardingCompleted_InitLoad_ShouldBeFalse() {
+
+		val viewModel = SplashViewModel(
+			useCases = dataStoreUseCases,
+			defaultDispatcher = testDispatcher
+		)
+
+		assertFalse(viewModel.onBoardingCompleted.value)
+	}
+
+	@Test
+	fun onBoardingCompleted_UpdateValue_ShouldBeTrue() = runTest {
+		whenever(readOnBoardingUseCase()).thenReturn(flowOf(true))
+
+		val viewModel = SplashViewModel(
+			DataStoreUseCases(
+				readOnBoardingUseCase,
+				saveOnBoardingState,
+				languageProvider,
+				saveLanguageState
+			),
+			testDispatcher
+		)
+
+		viewModel.onBoardingCompleted.test {
+			assertFalse(awaitItem())
+			assertTrue(awaitItem())
+			cancelAndConsumeRemainingEvents()
+		}
 	}
 
 
