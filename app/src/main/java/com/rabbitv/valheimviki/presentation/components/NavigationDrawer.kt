f3fff368750af358e373d85308374e2abae7ffb2
RM <dwernicki.miochal@gmail.com>
2025-08-04 14:36:23 +0200

    Refactor: Improved NavigationDrawer item selection and performance
    - Replaced `Column` with `LazyColumn` in `DrawerContent` for better performance with many items.
    - Introduced `drawerId` to `DrawerItem` and used it for managing the selected item state.
    - Implemented `LaunchedEffect` to update the selected item based on the current navigation destination.
    - Refined `onItemClick` logic to prevent redundant navigation and ensure proper state restoration.
    - Updated `isSelected` in `DrawerNavigationItem` to be a lambda function for dynamic updates.
    - Removed unused `findSelectedDrawerItem` function.
    - Adjusted preview to reflect new `DrawerItem` structure and lambda parameters.

--- app/src/main/java/com/rabbitv/valheimviki/presentation/components/NavigationDrawer.kt
+++ app/src/main/java/com/rabbitv/valheimviki/presentation/components/NavigationDrawer.kt
@@ -3,7 +3,6 @@ package com.rabbitv.valheimviki.presentation.components
 import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,8 +10,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.HorizontalDivider
@@ -25,7 +24,13 @@ import androidx.compose.material3.NavigationDrawerItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -38,8 +43,10 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.composables.icons.lucide.Anvil
 import com.composables.icons.lucide.Cuboid
@@ -59,7 +66,6 @@ import com.rabbitv.valheimviki.R
 import com.rabbitv.valheimviki.navigation.GridDestination
 import com.rabbitv.valheimviki.navigation.ListDestination
 import com.rabbitv.valheimviki.navigation.NavigationDestination
-
 import com.rabbitv.valheimviki.ui.theme.ForestGreen10Dark
 import com.rabbitv.valheimviki.ui.theme.ForestGreen40Dark
 import com.rabbitv.valheimviki.ui.theme.PrimaryText
@@ -69,6 +75,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
 data class DrawerItem(
+	val drawerId: Int,
 	val iconPainter: Painter? = null,
 	val icon: ImageVector? = null,
 	val label: String,
@@ -83,33 +90,53 @@ fun NavigationDrawer(
 	scope: CoroutineScope,
 	childNavController: NavHostController,
 	items: List<DrawerItem>,
-	selectedItem: DrawerItem,
-	isDetailScreen: Boolean,
-	isTransitionActive: Boolean,
+	isDetailScreen: () -> Boolean,
+	isTransitionActive: () -> Boolean,
 	content: @Composable () -> Unit,
 ) {
+	val navBackStackEntry by childNavController.currentBackStackEntryAsState()
+	val currentDestination = navBackStackEntry?.destination
+
+	var selectedId by rememberSaveable { mutableIntStateOf(0) }
+
+	LaunchedEffect(currentDestination) {
+		items.firstOrNull { item ->
+			currentDestination
+				?.hierarchy
+				?.any { it.route == item.navigationDestination.toString() } == true
+		}?.let {
+			selectedId = it.drawerId
+		}
+	}
+
+	val onItemClick = remember(scope, childNavController) {
+		{ item: DrawerItem ->
+			val currentRoute = childNavController.currentBackStackEntry?.destination?.route
+			scope.launch { drawerState.close() }
+			if (item.navigationDestination.toString() != currentRoute) {
+				selectedId = item.drawerId
+				childNavController.navigate(item.navigationDestination) {
+					popUpTo(childNavController.graph.findStartDestination().id) {
+						saveState = true
+					}
+					launchSingleTop = true
+					restoreState = true
+				}
+			}
+
+		}
+	}
 	ModalNavigationDrawer(
 		modifier = modifier
 			.fillMaxSize()
 			.testTag("NavigationDrawer"),
 		drawerState = drawerState,
-		gesturesEnabled = isDetailScreen && !isTransitionActive,
+		gesturesEnabled = isDetailScreen() && !isTransitionActive(),
 		drawerContent = {
 			DrawerContent(
 				items = items,
-				selectedItem = selectedItem,
-				onItemClick = { item ->
-					if (item != selectedItem) {
-						childNavController.navigate(item.navigationDestination) {
-							popUpTo(childNavController.graph.findStartDestination().id) {
-								saveState = true
-							}
-							launchSingleTop = true
-							restoreState = true
-						}
-					}
-					scope.launch { drawerState.close() }
-				}
+				selectedItem = { selectedId },
+				onItemClick = onItemClick,
 			)
 		},
 	) {
@@ -120,31 +147,31 @@ fun NavigationDrawer(
 @Composable
 private fun DrawerContent(
 	items: List<DrawerItem>,
-	selectedItem: DrawerItem,
+	selectedItem: () -> Int,
 	onItemClick: (DrawerItem) -> Unit,
-) {
+
+	) {
 	ModalDrawerSheet(
 		modifier = Modifier.fillMaxWidth(0.92f),
 		drawerContainerColor = ForestGreen40Dark,
 	) {
-		Column(
-			Modifier.verticalScroll(rememberScrollState()),
+		LazyColumn(
 			verticalArrangement = Arrangement.Center,
 			horizontalAlignment = Alignment.CenterHorizontally
 		) {
-			DrawerHeader()
-			Spacer(Modifier.height(12.dp))
-			HorizontalDivider()
-			Spacer(Modifier.height(12.dp))
+			item("DrawerHeader") { DrawerHeader() }
+			item("DrawerHeaderSpacer") { Spacer(Modifier.height(12.dp)) }
+			item("DrawerHorizontalDivider") { HorizontalDivider() }
+			item("DrawerHorizontalDividerSpacer") { Spacer(Modifier.height(12.dp)) }
 
-			items.forEach { item ->
+			items(items, { item -> item.label + "drawerItem" }) { item ->
 				DrawerNavigationItem(
 					item = item,
-					isSelected = item == selectedItem,
-					onClick = { onItemClick(item) }
+					isSelected = { item.drawerId == selectedItem() },
+					onClick = { onItemClick(item) },
 				)
 			}
-			Spacer(Modifier.height(24.dp))
+			item("DrawerItemSpacer") { Spacer(Modifier.height(24.dp)) }
 		}
 	}
 }
@@ -177,7 +204,7 @@ private fun DrawerHeader() {
 @Composable
 private fun DrawerNavigationItem(
 	item: DrawerItem,
-	isSelected: Boolean,
+	isSelected: () -> Boolean,
 	onClick: () -> Unit,
 ) {
 	NavigationDrawerItem(
@@ -214,12 +241,28 @@ private fun DrawerNavigationItem(
 				fontSize = 16.sp,
 			)
 		},
-		selected = isSelected,
-		onClick = onClick,
+		selected = isSelected(),
+		onClick = {
+			onClick()
+		},
 		modifier = Modifier
 			.height(48.dp)
 			.padding(NavigationDrawerItemDefaults.ItemPadding)
 	)
+}
+
+private fun findSelectedDrawerItem(
+	currentRoute: String,
+	drawerItems: List<DrawerItem>
+): DrawerItem {
+	val allMatches = drawerItems.filter { item ->
+		val screenName = item.navigationDestination::class.simpleName ?: ""
+		screenName.isNotEmpty() && currentRoute.contains(screenName, ignoreCase = true)
+	}
+
+	return allMatches.maxByOrNull { item ->
+		(item.navigationDestination::class.simpleName ?: "").length
+	} ?: drawerItems.first()
 }
 
 @Preview(name = "NavigationDrawerImage", uiMode = Configuration.UI_MODE_NIGHT_YES)
@@ -248,48 +291,56 @@ private fun PreviewNavigationDrawer() {
 
 	val items = listOf(
 		DrawerItem(
+			drawerId = 0,
 			icon = Lucide.MountainSnow,
 			label = "Biomes",
 			contentDescription = "List of Biomes",
 			navigationDestination = GridDestination.WorldDestinations.BiomeGrid
 		),
 		DrawerItem(
+			drawerId = 1,
 			iconPainter = painterResource(R.drawable.boss_1),
 			label = "Bosses",
 			contentDescription = "Bosses section",
 			navigationDestination = GridDestination.CreatureDestinations.BossGrid
 		),
 		DrawerItem(
+			drawerId = 2,
 			iconPainter = painterResource(R.drawable.miniboss),
 			label = "MiniBosses",
 			contentDescription = "MiniBosses section",
 			navigationDestination = GridDestination.CreatureDestinations.MiniBossGrid
 		),
 		DrawerItem(
+			drawerId = 3,
 			icon = Lucide.Rabbit,
 			label = "Creatures",
 			contentDescription = "Creatures section",
 			navigationDestination = ListDestination.CreatureDestinations.MobList
 		),
 		DrawerItem(
+			drawerId = 4,
 			icon = Lucide.Swords,
 			label = "Weapons",
 			contentDescription = "Weapons section",
 			navigationDestination = ListDestination.ItemDestinations.WeaponList
 		),
 		DrawerItem(
+			drawerId = 5,
 			icon = Lucide.Shield,
 			label = "Armor",
 			contentDescription = "Armor section",
 			navigationDestination = ListDestination.ItemDestinations.ArmorList
 		),
 		DrawerItem(
+			drawerId = 6,
 			icon = Lucide.Utensils,
 			label = "Food",
 			contentDescription = "Food section",
 			navigationDestination = ListDestination.FoodDestinations.FoodList
 		),
 		DrawerItem(
+			drawerId = 7,
 			icon = Lucide.FlaskRound,
 			label = "Mead",
 			contentDescription = "Mead section",
@@ -297,42 +348,49 @@ private fun PreviewNavigationDrawer() {
 		),
 
 		DrawerItem(
+			drawerId = 8,
 			icon = Lucide.Anvil,
 			label = "Crafting Stations",
 			contentDescription = "Crafting Station section",
 			navigationDestination = ListDestination.CraftingDestinations.CraftingObjectsList
 		),
 		DrawerItem(
+			drawerId = 9,
 			icon = Lucide.Gavel,
 			label = "Tools",
 			contentDescription = "Tools section",
 			navigationDestination = ListDestination.ItemDestinations.ToolList
 		),
 		DrawerItem(
+			drawerId = 10,
 			icon = Lucide.Cuboid,
 			label = "Materials",
 			contentDescription = "Materials section",
 			navigationDestination = ListDestination.CraftingDestinations.MaterialCategory
 		),
 		DrawerItem(
+			drawerId = 11,
 			icon = Lucide.House,
 			label = "Building Materials",
 			contentDescription = "Building Materials section",
 			navigationDestination = ListDestination.CraftingDestinations.BuildingMaterialCategory
 		),
 		DrawerItem(
+			drawerId = 12,
 			icon = Lucide.Pickaxe,
 			label = "Ore Deposits",
 			contentDescription = "Ore Deposits section",
 			navigationDestination = GridDestination.WorldDestinations.OreDepositGrid
 		),
 		DrawerItem(
+			drawerId = 13,
 			icon = Lucide.Trees,
 			label = "Trees",
 			contentDescription = "Trees section",
 			navigationDestination = GridDestination.WorldDestinations.TreeGrid
 		),
 		DrawerItem(
+			drawerId = 14,
 			icon = Lucide.MapPinned,
 			label = "Points Of Interest",
 			contentDescription = "Points Of Interest section",
@@ -347,10 +405,9 @@ private fun PreviewNavigationDrawer() {
 			scope = rememberCoroutineScope(),
 			childNavController = rememberNavController(),
 			items = items,
-			selectedItem = items[0],
 			content = {},
-			isDetailScreen = false,
-			isTransitionActive = false,
+			isDetailScreen = { false },
+			isTransitionActive = { false },
 		)
 	}
 }
