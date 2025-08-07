c06017b20b2c88b29d4d880f14bc3094b04d0dc1
RM <dwernicki.miochal@gmail.com>
2025-08-07 13:03:42 +0200

    Refactor: MainAppBar `enabled` parameter to lambda (No more no needed recompose)
    - Changed the `enabled` parameter of `MainAppBar` from `Boolean` to `() -> Boolean`.
    - Updated the `IconButton`'s `enabled` property to call this lambda: `enabled = !enabled()`.
    - This allows for dynamic control of the navigation icon's enabled state based on a lambda expression rather than a static boolean value.

--- app/src/main/java/com/rabbitv/valheimviki/presentation/components/topbar/MainTopBar.kt
+++ app/src/main/java/com/rabbitv/valheimviki/presentation/components/topbar/MainTopBar.kt
@@ -29,83 +29,83 @@ import kotlinx.coroutines.launch
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainAppBar(
-    onSearchBarClick: () -> Unit = {},
-    onMenuClick: () -> Unit = {},
-    onBookMarkClick: () -> Unit = {},
-    scope: CoroutineScope,
-    drawerState: DrawerState,
-    enabled: Boolean = true
+	onSearchBarClick: () -> Unit = {},
+	onMenuClick: () -> Unit = {},
+	onBookMarkClick: () -> Unit = {},
+	scope: CoroutineScope,
+	drawerState: DrawerState,
+	enabled: () -> Boolean = { true }
 ) {
-    TopAppBar(
-        scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(),
-        colors = TopAppBarDefaults.topAppBarColors(
-            containerColor = Color.Transparent,
-            titleContentColor = MaterialTheme.colorScheme.primary,
-        ),
-        modifier = Modifier.testTag("HomeTopAppBar"),
-        navigationIcon = {
-            IconButton(
-                enabled = !enabled,
-                onClick = { scope.launch { drawerState.open() } },
-                modifier = Modifier
-                    .size(ICON_CLICK_DIM)
-            )
-            {
-                Icon(
-                    painter = painterResource(id = R.drawable.bars),
-                    contentDescription = "Menu section Icon",
-                    modifier = Modifier.size(ICON_SIZE)
-                )
-            }
-        },
-        title = {
-            Text(
-                "ValheimViki",
-                color = MaterialTheme.colorScheme.onPrimaryContainer,
-                style = MaterialTheme.typography.headlineLarge,
-            )
-        },
-        actions = {
-            IconButton(
-                onClick = { onBookMarkClick() },
-                modifier = Modifier
-                    .size(ICON_CLICK_DIM)
-            ) {
-                Icon(
-                    painter = painterResource(R.drawable.ic_bookmarks),
-                    contentDescription = "Bookmarks section Icon",
-                    modifier = Modifier.size(ICON_SIZE)
-                )
-            }
-            IconButton(
-                onClick = { onSearchBarClick() },
-                modifier = Modifier
-                    .size(ICON_CLICK_DIM)
-            ) {
-                Icon(
-                    painter = painterResource(R.drawable.icon_search),
-                    contentDescription = "Search section Icon",
-                    modifier = Modifier.size(ICON_SIZE)
-                )
-            }
-        }
+	TopAppBar(
+		scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(),
+		colors = TopAppBarDefaults.topAppBarColors(
+			containerColor = Color.Transparent,
+			titleContentColor = MaterialTheme.colorScheme.primary,
+		),
+		modifier = Modifier.testTag("HomeTopAppBar"),
+		navigationIcon = {
+			IconButton(
+				enabled = !enabled(),
+				onClick = { scope.launch { drawerState.open() } },
+				modifier = Modifier
+					.size(ICON_CLICK_DIM)
+			)
+			{
+				Icon(
+					painter = painterResource(id = R.drawable.bars),
+					contentDescription = "Menu section Icon",
+					modifier = Modifier.size(ICON_SIZE)
+				)
+			}
+		},
+		title = {
+			Text(
+				"ValheimViki",
+				color = MaterialTheme.colorScheme.onPrimaryContainer,
+				style = MaterialTheme.typography.headlineLarge,
+			)
+		},
+		actions = {
+			IconButton(
+				onClick = { onBookMarkClick() },
+				modifier = Modifier
+					.size(ICON_CLICK_DIM)
+			) {
+				Icon(
+					painter = painterResource(R.drawable.ic_bookmarks),
+					contentDescription = "Bookmarks section Icon",
+					modifier = Modifier.size(ICON_SIZE)
+				)
+			}
+			IconButton(
+				onClick = { onSearchBarClick() },
+				modifier = Modifier
+					.size(ICON_CLICK_DIM)
+			) {
+				Icon(
+					painter = painterResource(R.drawable.icon_search),
+					contentDescription = "Search section Icon",
+					modifier = Modifier.size(ICON_SIZE)
+				)
+			}
+		}
 
-    )
+	)
 }
 
 @Preview(
-    name = "HomeTopBar",
-    uiMode = Configuration.UI_MODE_NIGHT_YES
+	name = "HomeTopBar",
+	uiMode = Configuration.UI_MODE_NIGHT_YES
 )
 @Composable
 private fun PreviewHomeTopBar() {
-    ValheimVikiAppTheme { // Ensure your app theme is applied
-        MainAppBar(
-            onSearchBarClick = {},
-            onMenuClick = {},
-            onBookMarkClick = {},
-            scope = rememberCoroutineScope(),
-            drawerState = rememberDrawerState(DrawerValue.Closed)
-        )
-    }
+	ValheimVikiAppTheme { // Ensure your app theme is applied
+		MainAppBar(
+			onSearchBarClick = {},
+			onMenuClick = {},
+			onBookMarkClick = {},
+			scope = rememberCoroutineScope(),
+			drawerState = rememberDrawerState(DrawerValue.Closed)
+		)
+	}
 }
