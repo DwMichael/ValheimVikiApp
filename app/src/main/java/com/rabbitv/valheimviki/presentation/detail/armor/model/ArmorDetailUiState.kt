03b55cfdaa74e7d0a0ba1e9716d0b6cb432d2b5b
RM <dwernicki.miochal@gmail.com>
2025-08-12 21:13:44 +0200

    Add: Added ArmorDetailUiEvent.kt and Refactor ArmorUiState.kt
    - deleted isLoading and error state
    - convert materials and crafitngObject to UIState Object

---
+++ app/src/main/java/com/rabbitv/valheimviki/presentation/detail/armor/model/ArmorDetailUiState.kt
@@ -0,0 +1,13 @@
+package com.rabbitv.valheimviki.presentation.detail.armor.model
+
+import com.rabbitv.valheimviki.domain.model.armor.Armor
+import com.rabbitv.valheimviki.domain.model.crafting_object.CraftingObject
+import com.rabbitv.valheimviki.domain.model.ui_state.uistate.UIState
+import com.rabbitv.valheimviki.domain.model.upgrader.MaterialUpgrade
+
+data class ArmorDetailUiState(
+	val armor: Armor? = null,
+	val materials: UIState<List<MaterialUpgrade>> = UIState.Loading,
+	val craftingObject: UIState<CraftingObject?> = UIState.Loading,
+	val isFavorite: Boolean = false,
+)
