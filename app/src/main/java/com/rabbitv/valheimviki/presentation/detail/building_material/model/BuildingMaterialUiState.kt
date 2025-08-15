9c924ef1317510f5bb3e68e4b0edca205f2a3732
RM <dwernicki.miochal@gmail.com>
2025-08-15 19:00:30 +0200

    Refactor: Update BuildingMaterialUiState to use UIState for asynchronous data
    - Changed `materials`, `foods`, and `craftingStation` from simple lists to `UIState<List<*>>`. This allows for explicit representation of loading, success, and error states for these data fields.
    - Removed `isLoading` and `error` fields as their functionality is now encapsulated within the new `UIState` properties.

--- app/src/main/java/com/rabbitv/valheimviki/presentation/detail/building_material/model/BuildingMaterialUiState.kt
+++ app/src/main/java/com/rabbitv/valheimviki/presentation/detail/building_material/model/BuildingMaterialUiState.kt
@@ -2,13 +2,12 @@ package com.rabbitv.valheimviki.presentation.detail.building_material.model
 
 import com.rabbitv.valheimviki.domain.model.building_material.BuildingMaterial
 import com.rabbitv.valheimviki.domain.model.crafting_object.CraftingObject
+import com.rabbitv.valheimviki.domain.model.ui_state.uistate.UIState
 
 data class BuildingMaterialUiState(
 	val buildingMaterial: BuildingMaterial? = null,
-	val materials: List<RequiredMaterial> = emptyList(),
-	val foods: List<RequiredFood> = emptyList(),
-	val craftingStation: List<CraftingObject> = emptyList(),
+	val materials: UIState<List<RequiredMaterial>> = UIState.Loading,
+	val foods: UIState<List<RequiredFood>> = UIState.Loading,
+	val craftingStation: UIState<List<CraftingObject>> = UIState.Loading,
 	val isFavorite: Boolean = false,
-	val isLoading: Boolean = false,
-	val error: String? = null
 )
