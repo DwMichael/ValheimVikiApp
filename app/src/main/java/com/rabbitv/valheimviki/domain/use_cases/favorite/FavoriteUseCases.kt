176d0f32c594781fdca12d599b6795988ff74f11
RM <dwernicki.miochal@gmail.com>
2025-08-16 16:55:24 +0200

    Refactor: Introduce ToggleFavoriteUseCase
    - Added `ToggleFavoriteUseCase` to `FavoriteUseCases` data class.
    - Updated `FavoriteViewModelTest` to include and mock `ToggleFavoriteUseCase`.

--- app/src/main/java/com/rabbitv/valheimviki/domain/use_cases/favorite/FavoriteUseCases.kt
+++ app/src/main/java/com/rabbitv/valheimviki/domain/use_cases/favorite/FavoriteUseCases.kt
@@ -4,6 +4,7 @@ import com.rabbitv.valheimviki.domain.use_cases.favorite.add_to_favorite.AddFavo
 import com.rabbitv.valheimviki.domain.use_cases.favorite.delete_from_favorite.DeleteFavoriteUseCase
 import com.rabbitv.valheimviki.domain.use_cases.favorite.get_all_favorite_items.GetAllFavoritesUseCase
 import com.rabbitv.valheimviki.domain.use_cases.favorite.is_favorite.IsFavoriteUseCase
+import com.rabbitv.valheimviki.domain.use_cases.favorite.toggle_favorite.ToggleFavoriteUseCase
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -12,5 +13,6 @@ data class FavoriteUseCases @Inject constructor(
 	val isFavorite: IsFavoriteUseCase,
 	val getAllFavoritesUseCase: GetAllFavoritesUseCase,
 	val deleteFavoriteUseCase: DeleteFavoriteUseCase,
-	val addFavoriteUseCase: AddFavoriteUseCase
+	val addFavoriteUseCase: AddFavoriteUseCase,
+	val toggleFavoriteUseCase: ToggleFavoriteUseCase
 )
