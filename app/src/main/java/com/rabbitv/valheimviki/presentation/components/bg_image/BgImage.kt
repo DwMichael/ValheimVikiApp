91e9fac3a94aa7a8385c8a7003c1b427ad7094e1
RM <dwernicki.miochal@gmail.com>
2025-08-18 18:41:09 +0200

    Refactor BgImage: Make remember Bitmap of image resource -Reduced work on CPU

--- app/src/main/java/com/rabbitv/valheimviki/presentation/components/bg_image/BgImage.kt
+++ app/src/main/java/com/rabbitv/valheimviki/presentation/components/bg_image/BgImage.kt
@@ -1,20 +1,32 @@
 package com.rabbitv.valheimviki.presentation.components.bg_image
 
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.FilterQuality
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.imageResource
 import com.rabbitv.valheimviki.R
 
 @Composable
 fun BgImage() {
-	val painterBackgroundImage = painterResource(R.drawable.main_background)
-	Image(
-		painter = painterBackgroundImage,
-		contentDescription = "BackgroundImage",
-		contentScale = ContentScale.Crop,
-		modifier = Modifier.fillMaxSize(),
+	val img = ImageBitmap.imageResource(R.drawable.main_background)
+//	val painter = painterResource(R.drawable.main_background)
+	val painter = remember(img) {
+		BitmapPainter(img, filterQuality = FilterQuality.Low)
+	}
+
+	Box(
+		Modifier
+			.fillMaxSize()
+			.paint(
+				painter = painter,
+				contentScale = ContentScale.Crop
+			)
 	)
 }
