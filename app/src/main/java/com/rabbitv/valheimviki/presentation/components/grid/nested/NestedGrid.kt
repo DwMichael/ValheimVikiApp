05046f54adc87affd2e331f10df2cbc1e692fc0a
RM <dwernicki.miochal@gmail.com>
2025-08-18 19:01:09 +0200

    Added : NestedGrid (Its grid that can be placed in LazyList)
    - LazyVerticalGrid with ability to be put in LazyList
    - Limitation 1000 height

---
+++ app/src/main/java/com/rabbitv/valheimviki/presentation/components/grid/nested/NestedGrid.kt
@@ -0,0 +1,52 @@
+package com.rabbitv.valheimviki.presentation.components.grid.nested
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.rabbitv.valheimviki.domain.repository.Droppable
+import com.rabbitv.valheimviki.ui.theme.MEDIUM_PADDING
+
+@Stable
+data class NestedItems<T>(
+	val items: List<T>
+)
+
+@Composable
+fun <T : Droppable> NestedGrid(
+	nestedItems: NestedItems<T>,
+	gridCells: Int = 3,
+	gridMaxHeight: Dp = 1000.dp,
+	gridItem: @Composable (T) -> Unit
+) {
+	val lazyGridState = rememberLazyGridState()
+
+	LazyVerticalGrid(
+		columns = GridCells.Fixed(gridCells),
+		state = lazyGridState,
+		modifier = Modifier
+			.heightIn(max = gridMaxHeight)
+			.padding(
+				MEDIUM_PADDING
+			),
+		userScrollEnabled = false,
+		verticalArrangement = Arrangement.spacedBy(12.dp),
+		horizontalArrangement = Arrangement.spacedBy(12.dp),
+	) {
+		itemsIndexed(
+			items = nestedItems.items,
+			key = { index, item -> "${item.itemDrop.id}#$index" },
+			contentType = { _, _ -> "grid_item" }
+		) { _, product ->
+			gridItem(product)
+		}
+	}
+}
