c6e682cdef172a6bfb6f48590e771c59268898d1
RM <dwernicki.miochal@gmail.com>
2025-08-19 22:03:23 +0200

    Refactor: Specify return type for GetBiomesByIdsUseCase
    - Explicitly defined the return type of function in GetBiomesByIdsUseCase as Flow<List<Biome>>.

--- app/src/main/java/com/rabbitv/valheimviki/domain/use_cases/biome/get_biomes_by_ids/GetBiomesByIdsUseCase.kt
+++ app/src/main/java/com/rabbitv/valheimviki/domain/use_cases/biome/get_biomes_by_ids/GetBiomesByIdsUseCase.kt
@@ -1,10 +1,12 @@
 package com.rabbitv.valheimviki.domain.use_cases.biome.get_biomes_by_ids
 
+import com.rabbitv.valheimviki.domain.model.biome.Biome
 import com.rabbitv.valheimviki.domain.repository.BiomeRepository
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GetBiomesByIdsUseCase @Inject constructor(
 	private val biomeRepository: BiomeRepository
 ) {
-	operator fun invoke(ids: List<String>) = biomeRepository.getBiomesByIds(ids)
+	operator fun invoke(ids: List<String>): Flow<List<Biome>> = biomeRepository.getBiomesByIds(ids)
 }
