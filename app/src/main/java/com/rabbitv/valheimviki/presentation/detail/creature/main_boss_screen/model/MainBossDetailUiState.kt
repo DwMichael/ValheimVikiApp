3d568e65680e3e2d351d844839ddbb30faab70cc
md <dwernicki.michal@gmail.com>
2025-08-20 16:43:13 +0200

    Refactor: MainBossDetailUiState.kt
    - now lists have they own UIStates

--- app/src/main/java/com/rabbitv/valheimviki/presentation/detail/creature/main_boss_screen/model/MainBossDetailUiState.kt
+++ app/src/main/java/com/rabbitv/valheimviki/presentation/detail/creature/main_boss_screen/model/MainBossDetailUiState.kt
@@ -4,16 +4,15 @@ import com.rabbitv.valheimviki.domain.model.biome.Biome
 import com.rabbitv.valheimviki.domain.model.creature.main_boss.MainBoss
 import com.rabbitv.valheimviki.domain.model.material.Material
 import com.rabbitv.valheimviki.domain.model.point_of_interest.PointOfInterest
+import com.rabbitv.valheimviki.domain.model.ui_state.uistate.UIState
 
 data class MainBossDetailUiState(
 	val mainBoss: MainBoss? = null,
+	val relatedBiome: Biome? = null,
 	val relatedForsakenAltar: PointOfInterest? = null,
 	val sacrificialStones: PointOfInterest? = null,
-	val dropItems: List<Material> = emptyList(),
-	val relatedSummoningItems: List<Material> = emptyList(),
-	val relatedBiome: Biome? = null,
+	val dropItems: UIState<List<Material>> = UIState.Loading,
+	val relatedSummoningItems: UIState<List<Material>> = UIState.Loading,
 	val trophy: Material? = null,
 	val isFavorite: Boolean = false,
-	val isLoading: Boolean = false,
-	val error: String? = null
 )
