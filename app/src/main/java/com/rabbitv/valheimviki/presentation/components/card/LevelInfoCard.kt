6d1feca184c172f68795fe1d431dc8331e3ce310
md <dwernicki.michal@gmail.com>
2025-08-20 12:08:10 +0200

    Refactor: Changed remember to rememberSaveable in LevelInfoCard

--- app/src/main/java/com/rabbitv/valheimviki/presentation/components/card/LevelInfoCard.kt
+++ app/src/main/java/com/rabbitv/valheimviki/presentation/components/card/LevelInfoCard.kt
@@ -32,6 +32,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -81,7 +82,7 @@ fun LevelInfoCard(
 	upgradeStats: List<GridLevelInfo> = emptyList(),
 	materialsForUpgrade: List<MaterialUpgrade> = emptyList(),
 	foodForUpgrade: List<FoodAsMaterialUpgrade> = emptyList(),
-	visibleContent: MutableState<Boolean> = remember { mutableStateOf(false) },
+	visibleContent: MutableState<Boolean> = rememberSaveable { mutableStateOf(false) },
 ) {
 
 	val filteredList = upgradeStats
