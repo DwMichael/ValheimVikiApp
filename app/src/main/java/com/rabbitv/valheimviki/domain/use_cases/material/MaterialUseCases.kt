f24cc5f35aaec896277deff4993624d2b8ad4c18
md <dwernicki.michal@gmail.com>
2025-08-20 16:39:12 +0200

    Refactor: Added New materialDao fun .
    - refactor repository repoImpl and useCase of material to work with new materialDao fun

--- app/src/main/java/com/rabbitv/valheimviki/domain/use_cases/material/MaterialUseCases.kt
+++ app/src/main/java/com/rabbitv/valheimviki/domain/use_cases/material/MaterialUseCases.kt
@@ -3,6 +3,7 @@ package com.rabbitv.valheimviki.domain.use_cases.material
 import com.rabbitv.valheimviki.domain.use_cases.material.get_local_Materials.GetLocalMaterialsUseCase
 import com.rabbitv.valheimviki.domain.use_cases.material.get_material_by_id.GetMaterialByIdUseCase
 import com.rabbitv.valheimviki.domain.use_cases.material.get_materials_by_ids.GetMaterialsByIdsUseCase
+import com.rabbitv.valheimviki.domain.use_cases.material.get_materials_by_subCategory_and_ids.GetMaterialsBySubCategoryIdsUseCase
 import com.rabbitv.valheimviki.domain.use_cases.material.get_materials_by_subcategory.GetMaterialsBySubCategoryUseCase
 import com.rabbitv.valheimviki.domain.use_cases.material.get_materials_by_subcategory_and_subtype.GetMaterialsBySubCategoryAndSubTypeUseCase
 import javax.inject.Inject
@@ -15,5 +16,6 @@ data class MaterialUseCases @Inject constructor(
 	val getMaterialsByIds: GetMaterialsByIdsUseCase,
 	val getMaterialById: GetMaterialByIdUseCase,
 	val getMaterialsBySubCategory: GetMaterialsBySubCategoryUseCase,
+	val getMaterialsBySubCategoryAndIds: GetMaterialsBySubCategoryIdsUseCase,
 	val getMaterialsBySubCategoryAndSubType: GetMaterialsBySubCategoryAndSubTypeUseCase,
 )
