c21ca23cbbe7f2336237e202056cc1376bd910d2
md <dwernicki.michal@gmail.com>
2025-08-20 23:26:02 +0200

    Refactor: NpcDetailUiState
    - Modify list to be UIState<List<..>>

--- app/src/main/java/com/rabbitv/valheimviki/presentation/detail/creature/npc/model/NpcDetailUiState.kt
+++ app/src/main/java/com/rabbitv/valheimviki/presentation/detail/creature/npc/model/NpcDetailUiState.kt
@@ -4,14 +4,15 @@ import com.rabbitv.valheimviki.domain.model.biome.Biome
 import com.rabbitv.valheimviki.domain.model.creature.npc.NPC
 import com.rabbitv.valheimviki.domain.model.material.Material
 import com.rabbitv.valheimviki.domain.model.point_of_interest.PointOfInterest
+import com.rabbitv.valheimviki.domain.model.ui_state.uistate.UIState
 
 data class NpcDetailUiState(
 	val npc: NPC? = null,
 	val biome: Biome? = null,
-	val shopItems: List<Material> = emptyList(),
-	val shopSellItems: List<Material> = emptyList(),
-	val hildirChests: List<Material> = emptyList(),
-	val chestsLocation: List<PointOfInterest> = emptyList(),
+	val shopItems: UIState<List<Material>> = UIState.Loading,
+	val shopSellItems: UIState<List<Material>> =  UIState.Loading,
+	val hildirChests:UIState< List<Material>> =  UIState.Loading,
+	val chestsLocation: UIState<List<PointOfInterest>> =  UIState.Loading,
 	val isFavorite: Boolean = false,
 	val isLoading: Boolean = false,
 	val error: String? = null
