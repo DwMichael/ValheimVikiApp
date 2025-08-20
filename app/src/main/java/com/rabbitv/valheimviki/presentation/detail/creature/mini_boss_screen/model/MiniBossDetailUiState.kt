852a04c2a22f1f5b78b495d94149340b0b876ef5
md <dwernicki.michal@gmail.com>
2025-08-20 20:18:27 +0200

    Refactor: MiniBossDetailUiState
    - Deleted isLoading
    - Deleted error

--- app/src/main/java/com/rabbitv/valheimviki/presentation/detail/creature/mini_boss_screen/model/MiniBossDetailUiState.kt
+++ app/src/main/java/com/rabbitv/valheimviki/presentation/detail/creature/mini_boss_screen/model/MiniBossDetailUiState.kt
@@ -3,12 +3,11 @@ package com.rabbitv.valheimviki.presentation.detail.creature.mini_boss_screen.mo
 import com.rabbitv.valheimviki.domain.model.creature.mini_boss.MiniBoss
 import com.rabbitv.valheimviki.domain.model.material.Material
 import com.rabbitv.valheimviki.domain.model.point_of_interest.PointOfInterest
+import com.rabbitv.valheimviki.domain.model.ui_state.uistate.UIState
 
 data class MiniBossDetailUiState(
 	val miniBoss: MiniBoss? = null,
 	val primarySpawn: PointOfInterest? = null,
-	val dropItems: List<Material> = emptyList(),
-	val isFavorite: Boolean = false,
-	val isLoading: Boolean = false,
-	val error: String? = null
+	val dropItems: UIState<List<Material>> = UIState.Loading,
+	val isFavorite: Boolean = false
 )
