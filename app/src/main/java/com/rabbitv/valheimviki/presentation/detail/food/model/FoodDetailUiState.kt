49935d725242fa1d36e2383091c02ec590f4330c
md <dwernicki.michal@gmail.com>
2025-08-21 18:26:01 +0200

    Refactor: FoodDetailUiState to using UIState not loading and error variables

--- app/src/main/java/com/rabbitv/valheimviki/presentation/detail/food/model/FoodDetailUiState.kt
+++ app/src/main/java/com/rabbitv/valheimviki/presentation/detail/food/model/FoodDetailUiState.kt
@@ -2,13 +2,12 @@ package com.rabbitv.valheimviki.presentation.detail.food.model
 
 import com.rabbitv.valheimviki.domain.model.crafting_object.CraftingObject
 import com.rabbitv.valheimviki.domain.model.food.Food
+import com.rabbitv.valheimviki.domain.model.ui_state.uistate.UIState
 
 data class FoodDetailUiState(
 	val food: Food? = null,
 	val craftingCookingStation: CraftingObject? = null,
-	val foodForRecipe: List<RecipeFoodData> = emptyList(),
-	val materialsForRecipe: List<RecipeMaterialData> = emptyList(),
+	val foodForRecipe: UIState< List<RecipeFoodData>> = UIState.Loading,
+	val materialsForRecipe: UIState<List<RecipeMaterialData>> = UIState.Loading,
 	val isFavorite: Boolean = false,
-	val isLoading: Boolean = false,
-	val error: String? = null
 )
