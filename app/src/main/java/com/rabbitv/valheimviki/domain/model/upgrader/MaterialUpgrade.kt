2179d2d1f929de01f663b6eec2d3d696bf04756a
md <dwernicki.michal@gmail.com>
2025-08-25 13:17:48 +0200

    Refactor: Make MaterialUpgrade implement Droppable
    - Implement the Droppable interface in MaterialUpgrade.
    - Add itemDrop, chanceStarList, and droppableType properties.
    - Set droppableType to DroppableType.MATERIAL.

--- app/src/main/java/com/rabbitv/valheimviki/domain/model/upgrader/MaterialUpgrade.kt
+++ app/src/main/java/com/rabbitv/valheimviki/domain/model/upgrader/MaterialUpgrade.kt
@@ -1,8 +1,14 @@
 package com.rabbitv.valheimviki.domain.model.upgrader
 
 import com.rabbitv.valheimviki.domain.model.material.Material
+import com.rabbitv.valheimviki.domain.model.presentation.DroppableType
+import com.rabbitv.valheimviki.domain.repository.Droppable
 
 data class MaterialUpgrade(
 	val material: Material,
-	val quantityList: List<Int?> = emptyList(),
-)
+	override val quantityList: List<Int?> = emptyList(),
+) : Droppable {
+	override val itemDrop: Material get() = material
+	override val chanceStarList: List<Int?> = emptyList()
+	override val droppableType: DroppableType = DroppableType.MATERIAL
+}
