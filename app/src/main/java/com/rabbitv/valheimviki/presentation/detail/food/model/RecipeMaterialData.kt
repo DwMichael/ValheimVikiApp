01b31a3af9f9afb9ca239a8825c91f2c8aa81237
md <dwernicki.michal@gmail.com>
2025-08-25 13:18:50 +0200

    Refactor: Generalize `RecipeMaterialData` to support any `ItemData`
    - Made `RecipeMaterialData` generic (`<out T : ItemData>`).
    - Changed `itemDrop` type from `Material` to `T`.
    - Inferred `droppableType` using `itemDrop.inferDropType()`.

--- app/src/main/java/com/rabbitv/valheimviki/presentation/detail/food/model/RecipeMaterialData.kt
+++ app/src/main/java/com/rabbitv/valheimviki/presentation/detail/food/model/RecipeMaterialData.kt
@@ -3,10 +3,12 @@ package com.rabbitv.valheimviki.presentation.detail.food.model
 import com.rabbitv.valheimviki.domain.model.material.Material
 import com.rabbitv.valheimviki.domain.model.presentation.DroppableType
 import com.rabbitv.valheimviki.domain.repository.Droppable
+import com.rabbitv.valheimviki.domain.repository.ItemData
+import com.rabbitv.valheimviki.utils.inferDropType
 
-data class RecipeMaterialData(
-	override val itemDrop: Material,
+data class RecipeMaterialData<out T : ItemData>(
+	override val itemDrop: T,
 	override val quantityList: List<Int?> = emptyList(),
 	override val chanceStarList: List<Int?> = emptyList(),
-	override val droppableType: DroppableType = DroppableType.MATERIAL
+	override val droppableType: DroppableType = itemDrop.inferDropType()
 ) : Droppable
